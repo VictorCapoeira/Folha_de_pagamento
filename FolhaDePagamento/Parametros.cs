73d44ea4effd21a34298797e5212ed4cfca5b9eb
Victor Emanuel <victorgit28@outlook.com>
2025-04-19 09:44:24 -0300

    Criada classe parametro e suas variaveis

---
+++ FolhaDePagamento/Parametros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolhaDePagamento
+{
+    public class Parametros
+    {
+        public List<decimal> InssFaixas { get; set; }
+        public List<decimal> InssAliquotas { get; set; }
+        public List<decimal> IrrfFaixas { get; set; }
+        public List<decimal> IrrfAliquotas { get; set; }
+        public List<decimal> IrrfDeducoes { get; set; }
+        public decimal Fgts { get; set; }
+        public decimal SalarioFamilia { get; set; }
+        public decimal LimiteSalarioFamilia { get; set; }
+    }
+
+}
