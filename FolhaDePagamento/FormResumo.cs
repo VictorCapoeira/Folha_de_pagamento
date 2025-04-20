2de67b83b4ab87f7fa4808690d293aea8756373e
Victor Emanuel <victorgit28@outlook.com>
2025-04-20 10:12:00 -0300

    Desing provisorio

---
+++ FolhaDePagamento/FormResumo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolhaDePagamento
+{
+    public partial class FormResumo : Form
+    {
+        public FormResumo()
+        {
+            InitializeComponent();
+        }
+
+        private void FormResumo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvGanhos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
